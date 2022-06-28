ffa7994ece599925f7bee46c8a28c7557a64fbb5
“damoncai” <782099197@qq.com>
2022-06-28 18:06:23 +0800

    文档

--- step_1_C/chapter16_point_arr/mian.c
+++ step_1_C/chapter16_point_arr/mian.c
@@ -1,54 +1,64 @@
 #include<stdio.h>
 
-void test_01() {
-	int arr[5] = {1,2,3,4};
-	int i;
-	int *p;
-	p = &arr[0];
-	for(i = 0 ; i < 5 ; i++) {
-	
-		printf("%d\n",arr[i]);
-		printf("%d\n",*(p + i));
-	}
-}
-
-
-// 指针数组
-void test_02() {
-	int a = "10";
-	int b = "20";
-	int c = "30";
-
-
-	int *arr[3] = {&a,&b,&c};
-
-	char *arr2[3] = {"hehe","haha","heihei"};
-
-	printf("%s\n", arr2[1]);
-	printf("%c\n", *(arr2[1] + 1));
-
-}
-
-// 数组指针
-void test_03() {
-	int a[3][4] = {11,12,13,14,21,22,23,24,31,32,33,34};
-	int (*p)[4];
-
-	printf("%p\n",a);
-	printf("%p\n",a + 1);
-
-	p = a;
-	printf("%p\n",p);
-	printf("%p\n",p + 1);
-
-	printf("\n");
-	printf("%d\n", *(*(p + 0) + 1));
-
+void usart(char SBUF)
+{
+	static char topic[10] = ""; //topic
+	static char DATA[20] = ""; // 数据
+	static char DATA_INDEX = 0; // 数据下标
+	static char ing = 0; // 是否正在存储
+	static char package_len = 0x00; // 数据包长度
+	static char topic_len = 0x00; // topic长度
+	static char index = 0;
+	if(SBUF == 0x30 && ing == 0){ // MQTT接收报文
+			DATA[index++] = SBUF;
+			ing = 1;
+		}
+		
+		if(ing == 1 && index == 1){ // 报文长度
+			package_len = SBUF;
+			index ++;
+		}
+		
+		if(ing == 1 && index == 2 && SBUF == 0x00){
+			index ++;
+		}
+		
+		if(ing == 1 && index == 3){ // topic长度
+			topic_len = SBUF;
+			index++;
+		}
+			
+		if(ing == 1 && topic_len != 0x00) { // 收集topic
+			topic[index - 4] = SBUF;
+			index ++;
+			topic_len --;
+		}
+		
+		if(ing == 1 && topic_len == 0x00 && index >= 4 ){ //采集数据
+			DATA[DATA_INDEX] = SBUF;
+			DATA_INDEX++;
+			index++;
+			if((package_len + 2) >= index){// 采集结束
+				topic[10] = ""; //topic
+				DATA[20] = ""; // 数据
+				DATA_INDEX = 0; // 数据下标
+				ing = 0; // 是否正在存储
+				package_len = 0x00; // 数据包长度
+				0x00; // topic长度
+				index = 0;
+			}
+		}
+	printf("%s\n",topic);
 }
 
 int main() {
+	char arr[] = {0x30,0x07,0x00,0x04,0x74,0x64,0x73,0x74,0x31};
+	int i;
+	for(i = 0 ; i < 9 ; i++){
+		usart(arr[i]);
+	}
 	
-	test_03();
+
 	return 0;
 
 }
